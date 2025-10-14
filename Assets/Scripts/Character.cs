658119be84e408940e7c6cd668f1ebd0a1b1f88d
Rahvokkendov <rahvokkendov@gmail.com>
2025-10-14 16:50:26 +0700

    Blank Classes Update

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+
+    private int health;
+    public int Health 
+    { 
+      get { return health; } 
+      set { health = (value < 0 ? 0 : value); }
+    }
+
+    protected Animator anim;
+    protected Rigidbody2D rb;
+
+
+    public void TakeDamage(int damage)
+    {
+        Health -= damage;
+        Debug.Log($"{this.name} taken {damage} amounts of damage || Current Health is {Health}");
+
+        IsDead();
+    }
+
+    public bool IsDead()
+    {
+        if (Health <= 0)
+        {
+            Destroy(this.gameObject);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
