58f1005c97f1ac1d3d598ff11ed26a2a576864c6
Rahvokkendov <rahvokkendov@gmail.com>
2025-10-21 15:36:35 +0700

    Ant Patrol Border

--- Assets/Scripts/Ant.cs
+++ Assets/Scripts/Ant.cs
@@ -2,11 +2,20 @@ using UnityEngine;
 
 public class Ant : Enemy
 {
+    [SerializeField] Vector2 velocity;
+    public Transform[] MovePoints;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         base.Initialized(10);
     }
+
+    public override void Behavior()
+    {
+
+    }
+
 
     // Update is called once per frame
     void Update()
