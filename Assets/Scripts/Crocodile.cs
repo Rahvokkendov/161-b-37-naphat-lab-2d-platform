3aa82ad9a5828d21640d692d07fd2c7a8462ce27
Rahvokkendov <rahvokkendov@gmail.com>
2025-10-21 16:33:00 +0700

    Crocodile Detection

--- Assets/Scripts/Crocodile.cs
+++ Assets/Scripts/Crocodile.cs
@@ -2,15 +2,39 @@ using UnityEngine;
 
 public class Crocodile : Enemy
 {
+    [SerializeField] float atkRange;
+    public Player Player;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         base.Initialized(80);
+        DamageHit = 20;
+
+        atkRange = 6.0f;
+        Player = GameObject.FindFirstObjectByType<Player>();
     }
 
     public override void Behavior()
     {
-        throw new System.NotImplementedException();
+        Vector2 distance = transform.position - Player.transform.position;
+
+        if (distance.magnitude <= atkRange)
+        {
+            Debug.Log($"{Player.name} is in {this.name} range");
+        }
+        Shoot();
+    }
+
+    public void Shoot()
+    {
+        Debug.Log($"{this.name} is trowing rocks at {Player.name}");
+    }
+
+
+    private void FixedUpdate()
+    {
+        Behavior();
     }
 
     // Update is called once per frame
